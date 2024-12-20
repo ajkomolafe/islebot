6742e224e72763c71e13b9de66be2656ee3e8f3c
ajkomolafe <141524434+ajkomolafe@users.noreply.github.com>
2024-12-20 16:47:39 -0600

    can handle /np pp calculation for mania maps, to handle set links and recommendations

---
+++ islebeatmap.py
@@ -0,0 +1,71 @@
+import requests
+import os
+from dotenv import load_dotenv
+import ossapi
+import rosu_pp_py as rosu
+
+load_dotenv()
+
+client_id = int(os.getenv("CLIENT_ID"))
+client_secret = str(os.getenv("CLIENT_SECRET"))
+api = ossapi.Ossapi(client_id, client_secret)
+
+class Beatmap():
+    def __init__(self, beatmap_id):
+        difficulty = api.beatmap(beatmap_id)
+
+        self.version = difficulty.version
+        self.data = api.beatmapset(difficulty.beatmapset_id)
+
+        if not (difficulty.mode == ossapi.enums.GameMode.MANIA):
+            raise Exception("Not a mania difficulty")
+        if not (difficulty.status == ossapi.enums.RankStatus.RANKED or difficulty.status == ossapi.enums.RankStatus.APPROVED):
+            raise Exception("Difficulty is not ranked")
+        
+        difficulty_url = "https://osu.ppy.sh/osu/" + str(difficulty.id)
+
+        try:
+            filename = "./difficulties/" + str(difficulty.id) + ".osu"
+            # try:
+            #     os.remove(filename)
+            # except OSError:
+            #     pass
+
+            if not os.path.exists(filename): #dont overwrite if file has been downloaded, as they don't change
+                response = requests.get(difficulty_url)
+                with open(filename, "w", encoding=response.encoding) as file:
+                    lines = response.text.split("\n")
+                    for line in lines:
+                        file.write(line)
+                    # file.flush()
+
+            rosu_difficulty = None
+            with open(filename, encoding='utf-8') as file:
+                rosu_difficulty = rosu.Beatmap(content = file.read()) #doesn't have to be converted as mode is already in the file
+
+            #cache these using mongodb, as well as w mods etc
+            rosu_perf = rosu.Performance(accuracy = 100)
+            self.acc_100 = rosu_perf.calculate(rosu_difficulty).pp
+
+            rosu_perf.set_accuracy(99)
+            self.acc_99 = rosu_perf.calculate(rosu_difficulty).pp
+
+            rosu_perf.set_accuracy(97)
+            self.acc_97 = rosu_perf.calculate(rosu_difficulty).pp
+
+            rosu_perf.set_accuracy(95)
+            self.acc_95 = rosu_perf.calculate(rosu_difficulty).pp
+
+        except Exception as e:
+            raise e
+        
+
+def calculateBeatmapsetPerformances(beatmapset_id):
+    difficulties = api.beatmapset(beatmapset_id).beatmaps
+    beatmap_list = []
+
+    for difficulty in difficulties:
+        beatmap = Beatmap(difficulty.id)
+        beatmap_list.append(beatmap)
+
+    return beatmap_list
--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import os
 import irc.bot
 from dotenv import load_dotenv
+import re
+import islebeatmap
 
 class IsleBot(irc.bot.SingleServerIRCBot):
     def __init__(self, username, server, server_backup, port, password):
@@ -12,20 +14,74 @@ class IsleBot(irc.bot.SingleServerIRCBot):
         print("Connected to bancho.")
         #todo: check on if the thing rejoins on disconnect
 
+    # def on_welcome(self, c, e):
+    #     c.join("#osu")
+
+    # def _on_mode(self, c, e):
+    #     print(e)
+
+    def on_ctcp(self, c, e):
+        self.handle_beatmap_link(c, e, e.arguments[1])
+
+    # def on_pubmsg(self, c, e):
+    #     print(f"{e.source.nick}: {e.arguments[0].lower()}")
+    
+    def handle_beatmap_link(self, c, e, message):
+        osu_url_regex = r"https?://osu\.ppy\.sh/beatmapsets/[\da-zA-Z#/]*\b"
+        beatmapset_urls = re.findall(osu_url_regex, message)
+        if len(beatmapset_urls) >= 1:
+            beatmapset_url = beatmapset_urls[0]
+            if '#' in beatmapset_url: 
+                #Specific difficulty given
+                beatmap = None
+                try:
+                    hashtag_index = beatmapset_url.index('#')
+                    beatmap_id = int(beatmapset_url[hashtag_index+2:])
+                    beatmap = islebeatmap.Beatmap(beatmap_id)
+                    output_message = f"[{beatmapset_url} {beatmap.data.artist_unicode} - {beatmap.data.title_unicode} [{beatmap.version}]] | All MAX: {beatmap.acc_100:.2f}pp | 99%: {beatmap.acc_99:.2f}pp | 97%: {beatmap.acc_97:.2f}pp | 95%: {beatmap.acc_95:.2f}pp ♥"
+                    c.privmsg(e.source.nick, output_message)
+                    print(f"Message to {e.source.nick}: {output_message}")
+
+                except Exception as e:
+                    print("Error: " + repr(e))
+                    return
+                
+            else: 
+                #Beatmapset only
+                beatmapset_url = beatmapset_url.replace("https://", '').replace("http://", '')
+                beatmapset_url = beatmapset_url.replace("osu.ppy.sh/", '')
+                beatmapset_url = beatmapset_url.replace("beatmapsets/", '')
+                try:
+                    hashtag_index = beatmapset_url.index('#')
+                    beatmapset_url = beatmapset_url[:hashtag_index]
+                except Exception:
+                    print("Error: " + repr(e))
+                    return
+
     def on_privmsg(self, c, e):
-        if e.target == self.connection.get_nickname(): #if i am target of message
+        # if e.target == self.connection.get_nickname(): #if i am target of message
             sender = e.source.nick
-            message = e.arguments[0].lower()
+            input_message = e.arguments[0].lower()
+        
+            if '♥' in input_message: #from me
+                return
 
-            # Process the message (example: respond to "!hello")
-            if not message.startswith("!"):
+            self.handle_beatmap_link(c, e, e.arguments[0])
+
+            if (not input_message.startswith("!")) and sender != self.connection.get_nickname():
+                output_message = "Hi, I'm isle."
                 c.privmsg(sender, "Hi, I'm isle.")
+                print(f"Message to {e.source.nick}: {output_message}")
 
-            elif message.startswith("!h"): #!help
-                c.privmsg(sender, f"Hi, I'm isle. The only command I have currently is !r.")
+            elif input_message.startswith("!h"): #!help
+                output_message = "Hi, I'm isle. You can /np mania beatmaps or type !recommend for a recommend beatmap."
+                c.privmsg(sender, output_message)
+                print(f"Message to {e.source.nick}: {output_message}")
 
-            elif message.startswith("!r"): #!recommend
-                 c.privmsg(sender, f"Hi, I'm isle. The only command I have currently is !r.")
+            elif input_message.startswith("!r"): #!recommend
+                output_message = "Hi, I'm isle. This command is WIP."
+                c.privmsg(sender, output_message)
+                print(f"Message to {e.source.nick}: {output_message}")
 
 load_dotenv()
 username = str(os.getenv("SERVER_USERNAME")) #aka nickname/realname
@@ -34,7 +90,6 @@ server_backup = str(os.getenv("SERVER_BACKUP"))
 server_port = int(os.getenv("SERVER_PORT"))
 server_password = str(os.getenv("SERVER_PASSWORD"))
 l = [username, server, server_backup, server_port, server_password]
-print(l)
 
 isleBot = IsleBot(username, server, server_backup, server_port, server_password)
 isleBot.start()
